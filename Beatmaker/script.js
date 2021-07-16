e6990ba4ff07deb21dedbcb26d0d0cb19d9f3e95
DNIWE08 <olimp12345678@gmail.com>
2021-07-16 17:57:08 +0300

    feat: play/stop btn, animate pads

--- Beatmaker/script.js
+++ Beatmaker/script.js
@@ -6,25 +6,57 @@ class DrumKit {
     this.snareAudio = document.querySelector('.snare-sound');
     this.hihatAudio = document.querySelector('.hihat-sound');
     this.index = 0;
-    this.bpm = 80;
-  }
-
-  addClass() {
-    console.log(this);
+    this.bpm = 150;
+    this.isPlaying = null;
   }
 
   repeat() {
     let step = this.index % 8;
     const activePad = document.querySelectorAll(`.b${step}`);
-    console.log(activePad);
+    activePad.forEach((pad) => {
+      pad.style.animation = `0.3s ease 0s alternate 2 playTrack`;
+      if(pad.classList.contains('active')){
+        if(pad.classList.contains('kick-pad')) {
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+        if(pad.classList.contains('snare-pad')) {
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+        if(pad.classList.contains('hihat-pad')) {
+          this.hihatAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      }
+    });
     this.index++;
   }
 
   start() {
     const interval = (60 / this.bpm) * 1000;
-    setInterval(() => {
-      this.repeat()
-    }, interval)
+    if(this.isPlaying) {
+      clearInterval(this.isPlaying);
+      this.isPlaying = null;
+      this.index = 0;
+      this.playBtn.textContent = 'Play';
+    } else {
+      this.isPlaying = setInterval(() => {
+        this.repeat()
+      }, interval);
+      this.playBtn.textContent = 'Stop';
+    }
+  }
+
+  updateBtn() {
+    console.log(this.isPlaying);
+    if(this.isPlaying) {
+      this.playBtn.textContent = 'Stop';
+      this.playBtn.classList.remove('playing');
+    } else {
+      this.playBtn.textContent = 'Play';
+      this.playBtn.classList.add('playing');
+    }
   }
 
 }
@@ -32,6 +64,10 @@ class DrumKit {
 const drumKit = new DrumKit();
 
 drumKit.pads.forEach((pad) => {
-  pad.addEventListener('click', () => pad.classList.toggle('active'))
+  pad.addEventListener('click', () => pad.classList.toggle('active'));
+  pad.addEventListener('animationend', () => pad.style.animation = '');
 })
-drumKit.playBtn.addEventListener('click', () => drumKit.start())
+drumKit.playBtn.addEventListener('click', () => {
+  drumKit.start();
+  drumKit.updateBtn();
+});
